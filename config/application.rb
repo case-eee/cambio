d5779eda66ee9e344546d13e67afbc68a6e39296
Casey Ann Cumbow <cumbow8@gmail.com>
2015-03-17 21:34:52 -0500

    updates app file

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ Bundler.require(*Rails.groups)
 
 module Cambio
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join("templates")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -29,6 +30,6 @@ module Cambio
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
